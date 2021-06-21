de7de05e391c82274bfb61401fb8f84cd803f5b6
Randall Portuguez Hdz <57552205+rportu@users.noreply.github.com>
2021-06-21 17:37:46 -0600

    comUno” git commit -m comUno”

---
+++ AppBeverages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppBeverages
+//
+//  Created by Admin on 19/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
